e6a7b5bf95ad14483094b9d997e15ed8a813ef9b
Matteo Matrangolo <matteo.matrangolo@gmail.com>
2023-09-17 17:39:40 +0100

    Ship moving and shooting.

---
+++ settings.py
@@ -0,0 +1,14 @@
+class Settings:
+
+    def __init__(self):
+        self.screen_width = 5000
+        self.screen_height = 3400
+        self.bg_color = (230,230,230)
+        self.ship_speed = 6
+
+        # Bullet settings
+        self.bullet_speed = 10
+        self.bullet_width = 2
+        self.bullet_height = 50
+        self.bullet_color = (60,60,60)
+        self.bullets_allowed = 5
